efcd58abc1a1a0a37a2478d9cf432dd341c164ea
MonaMouawadDafagate <mona.mouawad@dafagate.com>
2024-11-05 17:54:45 +0200

    Initial Commit

---
+++ card_workoutTests/card_workoutTests.swift
@@ -0,0 +1,17 @@
+//
+//  card_workoutTests.swift
+//  card_workoutTests
+//
+//  Created by Mona on 05/11/2024.
+//
+
+import Testing
+@testable import card_workout
+
+struct card_workoutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ card_workoutUITests/card_workoutUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  card_workoutUITestsLaunchTests.swift
+//  card_workoutUITests
+//
+//  Created by Mona on 05/11/2024.
+//
+
+import XCTest
+
+final class card_workoutUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
