53f10033a55715b2c1e15493d13a173f529bc934
MonaMouawadDafagate <mona.mouawad@dafagate.com>
2025-02-05 11:49:04 +0200

    Initial commit

--- card_workout/CardSelectionVC.swift
+++ card_workout/CardSelectionVC.swift
@@ -9,21 +9,49 @@ import UIKit
 
 class CardSelectionVC: UIViewController {
 
+    @IBOutlet  var stopButton: UIButton!
+    
+    @IBOutlet weak var rulesButton: UIButton!
+    @IBOutlet weak var restartButton: UIButton!
+    @IBOutlet var buttons: [UIButton]!
+    @IBOutlet weak var cardImage: UIImageView!
+    
+    var timer : Timer!
+    var cards : [UIImage] =  CardImage.allCardsImage 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        startTimer()
         // Do any additional setup after loading the view.
+        for button in buttons{
+            button.layer.cornerRadius = 20
+        }
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self,
+                                     selector: #selector(showRandomImage), userInfo: nil, repeats: true)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @objc  func showRandomImage(){
+        cardImage.image = cards.randomElement() ?? UIImage(named: "AC")
     }
-    */
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
+    }
 
+    
+    @IBAction func restartActionButton(_ sender: UIButton) {
+        timer.invalidate()
+        startTimer()
+    }
+    @IBAction func rulesActionButton(_ sender: Any) {
+    }
+    @IBAction func stopAction(_ sender: UIButton) {
+        timer.invalidate()
+    }
 }
